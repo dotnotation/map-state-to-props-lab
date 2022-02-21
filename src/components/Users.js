6ab2566faf629ab72f486cfecf232a297dd46959
dotnotation <dotinmotionproductions@gmail.com>
2022-02-21 14:40:44 -0500

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-// add any needed imports here
-class Users extends Component {
+import { connect } from 'react-redux'
 
+
+class Users extends Component {
+  
   render() {
+    let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>)
     return (
       <div>
         <ul>
           Users!
+            {users}
+            Total number of users:
+            {this.props.numberOfUsers}
           {/* Write code here that displays the usernames of all users in the Redux store */}
           {/* In addition, display the total number of users curently in the store */}
         </ul>
@@ -16,6 +22,12 @@ class Users extends Component {
 }
 
 //add mapStateToProps here
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+    numberOfUsers: state.users.length
+  }
+}
 
 // connect this component to Redux
-export default Users
+export default connect(mapStateToProps)(Users)
